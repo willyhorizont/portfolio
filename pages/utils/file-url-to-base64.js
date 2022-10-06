9fd661b7da826f9a2cfb916c92a70f5130c6eaa9
hxrizxntw <willy.sby1016@gmail.com>
2022-10-07 02:38:33 +0700

    add main and fix style

--- pages/utils/file-url-to-base64.js
+++ pages/utils/file-url-to-base64.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import readBlob from '../../utils/readBlob';
 import Head from 'next/head';
+import readBlob from '../../utils/readBlob';
 import { BASE_PATH } from '../../configs/constants';
+import styles from '../../styles/Home.module.css';
 
 const FileUrlToBase64 = () => {
   const [fileUrl, setFileUrl] = useState('');
@@ -30,24 +31,26 @@ const FileUrlToBase64 = () => {
   };
 
   return (
-    <div style={{ padding: '8px' }}>
+    <div className={styles.container}>
       <Head>
         <title>File Url To Base64</title>
         <meta name="description" content="file url string to base64 string" />
         <link rel="icon" href={`${BASE_PATH}/favicon.ico`} />
       </Head>
 
-      {loading && <p>loading...</p>}
-      <h3 style={{ textAlign: 'center' }}>File Url String To Base64</h3>
-      <h4 style={{ textAlign: 'center' }}>File url string</h4>
-      <input value={fileUrl} onChange={getBase64String} style={{ width: '100%' }} />
-      <h4 style={{ textAlign: 'center' }}>Base64 string result</h4>
-      <textarea
-        value={base64String}
-        rows={20}
-        multiline
-        style={{ width: '100%', borderRadius: '8px', backgroundColor: '#091929', color: '#ddd' }}
-      />
+      <main className={styles.main}>
+        {loading && <p>loading...</p>}
+        <h3 style={{ textAlign: 'center' }}>File Url String To Base64</h3>
+        <h4 style={{ textAlign: 'center' }}>File url string</h4>
+        <input value={fileUrl} onChange={getBase64String} style={{ width: '100%' }} />
+        <h4 style={{ textAlign: 'center' }}>Base64 string result</h4>
+        <textarea
+          value={base64String}
+          rows={20}
+          multiline
+          style={{ width: '100%', borderRadius: '8px', backgroundColor: '#091929', color: '#ddd' }}
+        />
+      </main>
     </div>
   );
 };
