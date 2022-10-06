e5cf7167af9cc56f0065b023364a5b1d26c0132f
willyhorizont <86374235+willyhorizont@users.noreply.github.com>
2022-10-06 18:21:45 +0000

    Deploying to gh-pages from @ willyhorizont/portfolio@d6c2adce4eee52778781f7fe9a1ae196c4676941 🚀

--- _next/static/QlGj5AZ-9Fvus6s7BKM5O/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-72f0d13903a81b68.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
