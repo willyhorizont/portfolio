edad306b06bdfa34a86e5e2a36fa60b8dd9aeb6a
hxrizxntw <willy.sby1016@gmail.com>
2022-11-03 03:39:36 +0700

    update lat long scrapper 1103

--- pages/projects/lat-long-scrapper.js
+++ pages/projects/lat-long-scrapper.js
@@ -13,14 +13,11 @@ const LatLongScrapper = () => {
   const pacInputRef = useRef(null);
   const mapRef = useRef();
   const [objek, setObjek] = useState(null);
-  const [placesChanged, setPlaceChanged] = useState(false);
-  const placeChangedTimeoutRef = useRef(null);
   const router = useRouter();
-  const { kondisi } = router.query;
+  const { katakunci, kondisi } = router.query;
 
-  const setTimeOutCallback = () => {
-    setPlaceChanged(false);
-    clearTimeout(placeChangedTimeoutRef.current);
+  const handleEnterKey = (evt) => {
+    if (evt.keyCode === 13) setObjek(null);
   };
 
   useEffect(() => {
@@ -36,7 +33,6 @@ const LatLongScrapper = () => {
         restriction: { latLngBounds: indonesiaBound, strictBounds: false }
       });
       const searchBoxObj = new google.maps.places.SearchBox(pacInputRef.current, { componentRestrictions: { country: 'id' } });
-      // gmapObj.controls[google.maps.ControlPosition.TOP_LEFT].push(pacInputRef.current);
 
       gmapObj.addListener('bounds_changed', () => {
         // console.log('bounds_changed');
@@ -46,23 +42,10 @@ const LatLongScrapper = () => {
       let markerList = [];
 
       searchBoxObj.addListener('places_changed', () => {
-        setPlaceChanged(true);
-        placeChangedTimeoutRef.current = setTimeout(setTimeOutCallback, 1000);
         // console.log('places_changed');
         const pacInputValue = pacInputRef.current.value.toLowerCase();
         const placeList = searchBoxObj.getPlaces();
         // console.log('placeList', placeList);
-        const filteredPlaceList = placeList.filter((tempat) => {
-          if (kondisi === 'startsWith') {
-            console.log('masuk startsWith');
-            return tempat?.name?.toLowerCase?.()?.startsWith?.(pacInputValue);
-          }
-          return tempat?.name?.toLowerCase?.()?.includes?.(pacInputValue);
-        });
-        // console.log('filteredPlaceList', filteredPlaceList);
-        const newPlaceList = filteredPlaceList.map((tempat) => ({ name: tempat?.name, lat: tempat?.geometry?.location?.lat?.(), lng: tempat?.geometry?.location?.lng?.(), id: tempat?.place_id }));
-        // console.log('newPlaceList', newPlaceList);
-        setObjek(newPlaceList?.length === 0 ? null : newPlaceList);
 
         if (placeList.length === 0) return;
 
@@ -89,6 +72,14 @@ const LatLongScrapper = () => {
             }
           });
           gmapObj.fitBounds(bounds);
+
+          if (pacInputValue === katakunci) {
+            const filteredPlaceList = placeList.filter((tempat) => (kondisi === 'startsWith' ? tempat?.name?.toLowerCase?.()?.startsWith?.(pacInputValue) : tempat?.name?.toLowerCase?.()?.includes?.(pacInputValue)));
+            // console.log('filteredPlaceList', filteredPlaceList);
+            const newPlaceList = filteredPlaceList.map((tempat) => ({ name: tempat?.name, lat: tempat?.geometry?.location?.lat?.(), lng: tempat?.geometry?.location?.lng?.(), id: tempat?.place_id }));
+            // console.log('newPlaceList', newPlaceList);
+            setObjek(newPlaceList?.length === 0 ? null : newPlaceList);
+          }
           return;
         }
 
@@ -101,6 +92,12 @@ const LatLongScrapper = () => {
         if (place?.geometry?.viewport) gmapObj.fitBounds(place?.geometry?.viewport);
         gmapObj.setCenter(place?.geometry?.location);
         markerList.push(new google.maps.Marker({ map: gmapObj, title: place?.name, position: place?.geometry?.location }));
+
+        if (pacInputValue === katakunci && kondisi === 'startsWith' && place?.name?.toLowerCase?.()?.startsWith?.(pacInputValue)) {
+          setObjek({ name: place?.name, lat: place?.geometry?.location?.lat?.(), lng: place?.geometry?.location?.lng?.(), id: place?.place_id });
+          return;
+        }
+        if (pacInputValue === katakunci) setObjek({ name: place?.name, lat: place?.geometry?.location?.lat?.(), lng: place?.geometry?.location?.lng?.(), id: place?.place_id });
       });
     }).catch((err) => alert('Gagal memuat google maps karena: ', err));
   }, [kondisi]);
@@ -116,10 +113,9 @@ const LatLongScrapper = () => {
       <MainLayout>
         <Box sx={{ display: 'flex', position: 'relative', flexDirection: 'column' }}>
           <div style={{ zIndex: 1, borderRadius: '8px', flexGrow: 1, width: 'calc(100% - 16px)', position: 'absolute', top: '8px', left: 0, right: 0, margin: '0 auto', backgroundColor: 'black' }}>
-            <input ref={pacInputRef} placeholder="Masukkan Alamat" autoComplete="false" type="text" id="pacInputRef" name="pacInputRef" style={{ fontSize: '1.6rem', padding: '4px', borderRadius: '8px', color: 'white', width: '100%' }} />
+            <input ref={pacInputRef} onKeyDown={handleEnterKey} placeholder="Masukkan Alamat" autoComplete="false" type="text" id="pacInputRef" name="pacInputRef" style={{ fontSize: '1.6rem', padding: '4px', borderRadius: '8px', color: 'white', width: '100%' }} />
           </div>
           <div className="map" id="mapRef" style={{ height: '70vh', width: '100%', borderRadius: '8px', border: '1px solid', borderColor: 'divider' }} ref={mapRef} />
-          {placesChanged && <p id="placesChanged" style={{ textAlign: 'center' }}>places_changed!</p>}
           {objek !== null && (
             <div id="hasilSearch">
               <div style={{ display: 'flex', flexDirection: 'row', columnGap: '8px', justifyContent: 'space-between' }}>
@@ -140,6 +136,24 @@ const LatLongScrapper = () => {
               ))}
             </div>
           )}
+          {/* <div id="hasilSearch">
+            <div style={{ display: 'flex', flexDirection: 'row', columnGap: '8px', justifyContent: 'space-between' }}>
+              <div style={{ flex: 1 }}>Nama</div>
+              <div style={{ display: 'flex', flexDirection: 'row', flex: 1, justifyContent: 'space-between' }}>
+                <div>Lat</div>
+                <div>Lng</div>
+              </div>
+            </div>
+            {objek !== null && objek?.map?.((obj) => (
+              <div key={obj?.id} id="satuObj" style={{ display: 'flex', flexDirection: 'row', columnGap: '8px', justifyContent: 'space-between' }}>
+                <div id="namaObj" style={{ flex: 1 }}>{obj?.name}</div>
+                <div style={{ display: 'flex', flexDirection: 'row', flex: 1, justifyContent: 'space-between' }}>
+                  <div id="latObj">{obj?.lat}</div>
+                  <div id="lngObj">{obj?.lng}</div>
+                </div>
+              </div>
+            ))}
+          </div> */}
         </Box>
       </MainLayout>
     </>
